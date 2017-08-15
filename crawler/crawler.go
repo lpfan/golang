bd65f0f5daf574ed72222e5776088886af9281ec
rudy <misha.shchetinin@egmail.com>
2017-08-15 17:06:59 +0300

    Added traveling by pages

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -6,18 +6,29 @@ import (
     "github.com/PuerkitoBio/goquery"
 )
 
-const targetUrl = "http://www.bmwclub.ua/forums/60-%D0%9A%D1%83%D0%BF%D0%BB%D1%8F-%D0%9F%D1%80%D0%BE%D0%B4%D0%B0%D0%B6%D0%B0-%D0%91%D0%95%D0%97-%D0%9F%D0%A0%D0%90%D0%92%D0%98%D0%9B"
+const domainUrl = "http://bmwclub.ua"
+const targetUrlTemplate = "http://www.bmwclub.ua/forums/60-%D0%9A%D1%83%D0%BF%D0%BB%D1%8F-%D0%9F%D1%80%D0%BE%D0%B4%D0%B0%D0%B6%D0%B0-%D0%91%D0%95%D0%97-%D0%9F%D0%A0%D0%90%D0%92%D0%98%D0%9B"
 
 func main() {
-    doc, err := goquery.NewDocument(targetUrl)
-    if err != nil {
-        log.Fatal(err)
+
+    for pageNum := 0; pageNum < 10; pageNum++ {
+        targetUrl := targetUrlTemplate
+
+        if pageNum >= 2 {
+            targetUrl = targetUrlTemplate + "/page" + string(pageNum)
+        }
+
+        doc, err := goquery.NewDocument(targetUrl)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        doc.Find("#threads .threadbit a.title").Each(func(i int, s *goquery.Selection) {
+            topicLink, ok := s.Attr("href")
+            if ok {
+                fmt.Println(topicLink)
+            }
+        })
     }
 
-    doc.Find("#threads .threadbit a.title").Each(func(i int, s *goquery.Selection) {
-        topicLink, ok := s.Attr("href")
-        if ok {
-            fmt.Println(topicLink)
-        }
-    })
 }
