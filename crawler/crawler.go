e64d70c10f6ccd3e3d9ad71837755c3327556436
rudy <misha.shchetinin@gmail.com>
2017-08-21 00:26:04 +0300

    Fixed page number

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -9,9 +9,11 @@ import (
     "strings"
     "io/ioutil"
     "time"
+    "sync"
+    "strconv"
 )
 
-const domainUrl = "http://bmwclub.ua/"
+const domainUrl = "http://www.bmwclub.ua/"
 const targetUrlTemplate = "http://www.bmwclub.ua/forums/60-%D0%9A%D1%83%D0%BF%D0%BB%D1%8F-%D0%9F%D1%80%D0%BE%D0%B4%D0%B0%D0%B6%D0%B0-%D0%91%D0%95%D0%97-%D0%9F%D0%A0%D0%90%D0%92%D0%98%D0%9B"
 
 type  Topic struct {
@@ -31,16 +33,14 @@ func decodeStringToUtf(rawString string) string {
   return string(buf)
 }
 
-func topicWorker(s *mgo.Session, topicChannel<-chan string) {
-  for {
-    select {
-      case topicLink := <- topicChannel:
+func topicWorker(s *mgo.Session, wg *sync.WaitGroup, topicLink string) {
+        defer wg.Done()
+
         targetUrl := domainUrl + topicLink
         doc, err := goquery.NewDocument(targetUrl)
         if err != nil {
           log.Fatal(err)
         }
-        targetUrl = decodeStringToUtf(targetUrl)
 
         var topicHeader string
         topicHeader = doc.Find("div#postlist #posts div.postdetails div.postbody div.postrow h2.title").First().Text()
@@ -70,34 +70,11 @@ func topicWorker(s *mgo.Session, topicChannel<-chan string) {
         if mongoErr != nil {
           log.Fatal(mongoErr)
         }
-      }
-  }
 }
 
 func main() {
-    var topicChannel = make(chan string)
+    var wg sync.WaitGroup
 
-
-    var topics []string
-    for pageNum := 1; pageNum <= 5; pageNum++ {
-        targetUrl := targetUrlTemplate
-
-        if pageNum >= 2 {
-            targetUrl = targetUrlTemplate + "/page" + string(pageNum)
-        }
-
-        doc, err := goquery.NewDocument(targetUrl)
-        if err != nil {
-            log.Fatal(err)
-        }
-
-        doc.Find("#threads .threadbit a.title").Each(func(i int, s *goquery.Selection) {
-            topicLink, ok := s.Attr("href")
-            if ok {
-                topics = append(topics, decodeStringToUtf(topicLink))
-            }
-        })
-    }
     session, mongoErr := mgo.Dial("localhost")
     if mongoErr != nil {
       log.Fatal(mongoErr)
@@ -105,12 +82,33 @@ func main() {
     defer session.Close()
     session.SetMode(mgo.Monotonic, true)
 
-    for wCount := 0; wCount < 30; wCount++ {
-      go topicWorker(session, topicChannel)
-    }
+    for pageNum := 1; pageNum <= 5; pageNum++ {
+        targetUrl := targetUrlTemplate
 
-    for _, topic := range topics {
-      topicChannel <- topic
-    }
+        if pageNum >= 2 {
+            targetUrl = targetUrlTemplate + "/page" + strconv.Itoa(pageNum)
+        }
 
+        doc, err := goquery.NewDocument(targetUrl)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        doc.Find("div#threadlist div ol#threads .threadbit div.threadinfo div.inner h3.threadtitle a.title").Each(func(i int, s *goquery.Selection) {
+          topicLink, ok := s.Attr("href")
+          if ok {
+            topicLink = decodeStringToUtf(topicLink)
+            wg.Add(1)
+            go topicWorker(session, &wg, topicLink)
+          }
+        })
+        //topicLink, ok := doc.Find("div#threadlist div ol#threads .threadbit div.threadinfo div.inner h3.threadtitle a.title").First().Attr("href")
+        //if ok {
+        //  topicLink = decodeStringToUtf(topicLink)
+        //  wg.Add(1)
+        //  go topicWorker(session, &wg, topicLink)
+        //}
+        log.Printf("Processed %d page", pageNum)
+    }
+    wg.Wait()
 }
